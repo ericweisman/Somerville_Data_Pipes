2724b1cb50434a3ac7883189b9f83f38bb5da20c
DanielHadley <dphnrome@yahoo.com>
2016-03-17 19:32:32 -0400

    bat file

---
+++ daily.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\R\R-3.1.2\bin\R.exe" CMD BATCH --vanilla --slave "C:\Users\dhadley\Documents\GitHub\Somerville_Data_Pipes\website-analytics.R"
+"C:\Program Files\R\R-3.1.2\bin\R.exe" CMD BATCH --vanilla --slave "C:\Users\dhadley\Documents\GitHub\Somerville_Data_Pipes\police.R"
+"C:\Program Files\R\R-3.1.2\bin\R.exe" CMD BATCH --vanilla --slave "C:\Users\dhadley\Documents\GitHub\Somerville_Data_Pipes\inspectional-services.R"
+"C:\Program Files\R\R-3.1.2\bin\R.exe" CMD BATCH --vanilla --slave "C:\Users\dhadley\Documents\GitHub\Somerville_Data_Pipes\constituent-services.R"
