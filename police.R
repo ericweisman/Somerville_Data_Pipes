d881b3e852dec7cbd4ca50aabac3f01cb1d8e94b
Daniel Hadley <dphnrome@gmail.com>
2017-01-31 14:02:28 -0700

    woops Im not testing on site so I missed the write csv

--- police.R
+++ police.R
@@ -13,29 +13,29 @@ library(data.table)
 
 qol <- fread("https://data.somervillema.gov/api/views/n5sm-r6zx/rows.csv?accessType=DOWNLOAD")
 
-writeLines(qol,'//fileshare1/Departments2/Somerstat Data/Police/daily/QualityOfLife.csv')
-writeLines(qol,'//fileshare1/Departments2/Somerstat Data/Police/daily/data_pipeline_pls_dont_use/QualityOfLife.csv')
+write.csv(qol,'//fileshare1/Departments2/Somerstat Data/Police/daily/QualityOfLife.csv')
+write.csv(qol,'//fileshare1/Departments2/Somerstat Data/Police/daily/data_pipeline_pls_dont_use/QualityOfLife.csv')
 
 
 
 ci <- fread("https://data.somervillema.gov/api/views/4jey-jqxb/rows.csv?accessType=DOWNLOAD")
 
-writeLines(ci, "//fileshare1/Departments2/Somerstat Data/Police/daily/CriminalIncidents.csv")
-writeLines(ci,'//fileshare1/Departments2/Somerstat Data/Police/daily/data_pipeline_pls_dont_use/CriminalIncidents.csv')
+write.csv(ci, "//fileshare1/Departments2/Somerstat Data/Police/daily/CriminalIncidents.csv")
+write.csv(ci,'//fileshare1/Departments2/Somerstat Data/Police/daily/data_pipeline_pls_dont_use/CriminalIncidents.csv')
 
 
 
 mvc <- fread("https://data.somervillema.gov/api/views/3md9-rv67/rows.csv?accessType=DOWNLOAD")
 
-writeLines(mvc,'//fileshare1/Departments2/Somerstat Data/Police/daily/MotorVehicleCitations.csv')
-writeLines(mvc,'//fileshare1/Departments2/Somerstat Data/Police/daily/data_pipeline_pls_dont_use/MotorVehicleCitations.csv')
+write.csv(mvc,'//fileshare1/Departments2/Somerstat Data/Police/daily/MotorVehicleCitations.csv')
+write.csv(mvc,'//fileshare1/Departments2/Somerstat Data/Police/daily/data_pipeline_pls_dont_use/MotorVehicleCitations.csv')
 
 
 
 te <- fread("https://data.somervillema.gov/api/views/j2bq-38ev/rows.csv?accessType=DOWNLOAD")
 
-writeLines(te, "//fileshare1/Departments2/Somerstat Data/Police/daily/TrafficEnforcement.csv")
-writeLines(te,'//fileshare1/Departments2/Somerstat Data/Police/daily/data_pipeline_pls_dont_use/TrafficEnforcement.csv')
+write.csv(te, "//fileshare1/Departments2/Somerstat Data/Police/daily/TrafficEnforcement.csv")
+write.csv(te,'//fileshare1/Departments2/Somerstat Data/Police/daily/data_pipeline_pls_dont_use/TrafficEnforcement.csv')
 
 
 
